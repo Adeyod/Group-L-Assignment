0249fcaad9826a2212c52e7d97ccf0a5d82d6eb6
Adeyod <36342338+Adeyod@users.noreply.github.com>
2023-10-17 10:56:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const providerLogo = document.querySelector('.provider-logo');
 
 // Function to detect and set the provider logo
 function detectProvider(phoneNumber) {
-  const mtnPattern = /^(0703|0803|081|0903|0806|0706|\+234703|\+234803)/;
+  const mtnPattern =  /^(0703|0803|0810|0903|0806|0706|\+234703|\+234803|\+234806|\+234703|\+234810|\+234903|\+234706)/;
   const gloPattern = /^(0705|0805|0807|0815|0905|\+234705|\+234805|\+234807|\+234815|\+234905)/;
   const airtelPattern = /^(0701|0802|0808|0812|0901|\+234701|\+234802|\+234808|\+234812|\+234901)/;
   const nineMobilePattern = /^(0809|0817|0818|0909)/;
